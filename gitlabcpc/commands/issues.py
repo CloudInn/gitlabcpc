7a845632e2fc80469e0c48e772b7425c57b3bb8c
Abdelrahman Ghareeb <aghareeb@cloudinn.net>
2018-03-01 20:43:46 +0200

    Fixing linting problems.

--- gitlabcpc/commands/issues.py
+++ gitlabcpc/commands/issues.py
@@ -1,6 +1,8 @@
 from cement.core.controller import CementBaseController, expose
-from prompts import *
-from misc import *
+from prompts import IssuesExportClosedPrompt, IssuesExportLabelPrompt,\
+                    IssuesExportMilestonePrompt, IssuesExportProjectPrompt,\
+                    LabelNamePrompt, MilestoneNamePrompt,\
+                    IssueCloseConfirmationPrompt
 import datetime
 import sys
 import csv
@@ -28,7 +30,9 @@ class IssuesController(CementBaseController):
         if proceed == 'yes':
             for project in self.app.gl.projects.all(per_page=100):
                 closed = 0
-                for issue in project.issues.list(milestone=milestone, labels=[label_name], state='opened', per_page=100):
+                for issue in project.issues.list(milestone=milestone,
+                                                 labels=[label_name],
+                                                 state='opened', per_page=100):
                     issue.state_event = 'close'
                     issue.save()
                     closed += 1
@@ -41,17 +45,19 @@ class IssuesController(CementBaseController):
         milestone = IssuesExportMilestonePrompt().input
         only_opened = IssuesExportClosedPrompt().input
         now = datetime.datetime.now()
-        export_file = open('gitlab-issues-export-'+now.isoformat()[0:-7]+'.csv', 'w')
-        fields = ['id', 'project', 'title', 'description', 'labels', 'milestone', 'time spent',
-                'time estimated', 'author', 'assignee', 'due date', 'created',
-                'closed', 'last modified', 'confidential', 'state', 'upvotes', 'downvotes']
+        export_file = open('gitlab-issues-'+now.isoformat()[0:-7]+'.csv', 'w')
+        fields = ['id', 'project', 'title', 'description',
+                  'labels', 'milestone', 'time spent', 'time estimated',
+                  'author', 'assignee', 'due date', 'created',
+                  'closed', 'last modified', 'confidential',
+                  'state', 'upvotes', 'downvotes']
         writer = csv.DictWriter(export_file, fieldnames=fields)
         writer.writeheader()
 
         if label == '' and milestone == '' and project_name == '':
             print("All issues from all projects will be exported")
         if not project_name == '':
-            if not ',' in project_name:
+            if ',' not in project_name:
                 projects = self.app.gl.projects.list(search=project_name)
             else:
                 projects = []
@@ -62,9 +68,14 @@ class IssuesController(CementBaseController):
         for project in projects:
             print('Exporting for project: %s' % project.name)
             if only_opened == 'opened':
-                pages = project.issues.list(as_list=False, labels=[label], milestone=milestone, state='opened')
+                pages = project.issues.list(as_list=False,
+                                            labels=[label],
+                                            milestone=milestone,
+                                            state='opened')
             else:
-                pages = project.issues.list(as_list=False, labels=[label], milestone=milestone)
+                pages = project.issues.list(as_list=False,
+                                            labels=[label],
+                                            milestone=milestone)
             i = 0
             while i < pages.total:
                 issue = pages.next()
@@ -78,16 +89,24 @@ class IssuesController(CementBaseController):
                     assignee = assignee['username']
                 else:
                     assignee = ''
-                writer.writerow({'id': issue.iid, 'project': project.name, 'title': issue.attributes['title'],
-                    'description': issue.attributes['description'], 'labels': ';'.join(issue.attributes['labels']),
-                    'milestone': milestone_title, 'time spent': issue.attributes['time_stats']['total_time_spent'],
-                    'time estimated': issue.attributes['time_stats']['time_estimate'],
-                    'author': issue.attributes['author']['username'], 'assignee': assignee,
-                    'due date': issue.attributes['due_date'], 'created': issue.attributes['created_at'],
-                    'closed': issue.attributes['closed_at'],
-                    'last modified': issue.attributes['updated_at'], 'confidential': issue.attributes['confidential'],
-                    'state': issue.state, 'upvotes': issue.attributes['upvotes'], 'downvotes': issue.attributes['upvotes'] })
+                writer.writerow({'id': issue.iid,
+                                 'project': project.name,
+                                 'title': issue.attributes['title'],
+                                 'description': issue.attributes['description'],
+                                 'labels': ';'.join(issue.attributes['labels']),
+                                 'milestone': milestone_title,
+                                 'time spent': issue.attributes['time_stats']['total_time_spent'],
+                                 'time estimated': issue.attributes['time_stats']['time_estimate'],
+                                 'author': issue.attributes['author']['username'],
+                                 'assignee': assignee,
+                                 'due date': issue.attributes['due_date'],
+                                 'created': issue.attributes['created_at'],
+                                 'closed': issue.attributes['closed_at'],
+                                 'last modified': issue.attributes['updated_at'],
+                                 'confidential': issue.attributes['confidential'],
+                                 'state': issue.state,
+                                 'upvotes': issue.attributes['upvotes'],
+                                 'downvotes': issue.attributes['upvotes']})
                 i = i + 1
             print('issues: %i' % i)
         print('Export results saved at: %s' % export_file.name)
-
